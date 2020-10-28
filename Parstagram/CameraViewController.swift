d15ebc1ccc9e2114a6842722ec8f073b58a0f61a
Aman Dhruva Thamminana <dhruva.aman@gmail.com>
2020-10-28 00:52:26 -0400

    changez

--- Parstagram/CameraViewController.swift
+++ Parstagram/CameraViewController.swift
@@ -23,23 +23,22 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
     }
     
     @IBAction func onSubmit(_ sender: Any) {
-        let post = PFObject(className: "Posts")
-        post["caption"] = commentField.text
-        post["author"] = PFUser.current()!
-        
-        let imageData = imageView.image!.pngData()
-        let file = PFFileObject(data: imageData!)
-        post["image"] = file
-        
-        post.saveInBackground { (success, error) in
-            if success{
-                self.dismiss(animated: true, completion: nil)
-                print("Saved!")
-            }
-            else{
-                print("Error")
-            }
-        }
+        let post =  PFObject(className: "post")
+               post["caption"] = commentField.text
+               post["author"] = PFUser.current()!
+               let imageData = imageView.image?.pngData()
+               let file = PFFileObject(name: "image.png", data: imageData!)
+               post["image"] = file
+               post.saveInBackground {  (success, error) in
+                   if (success){
+                       self.dismiss(animated: true, completion: nil)
+                       print("saved")
+                   }
+                   else{
+                       print("Error")
+                   }
+                   
+               }
     }
     
     @IBAction func onCameraButton(_ sender: Any) {
